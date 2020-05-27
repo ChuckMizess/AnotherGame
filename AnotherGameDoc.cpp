5399d2f43129b2967f1c0ffce1bb9fc5efb2e719
Vital V <you@example.com>
2020-05-27 22:40:22 +0300

    ошибка недопустимый тип

--- AnotherGameDoc.cpp
+++ AnotherGameDoc.cpp
@@ -1,138 +1,38 @@
 ﻿
-// AnotherGameDoc.cpp: реализация класса CAnotherGameDoc 
-//
 
+// Команды CAnotherGameDoc
 #include "pch.h"
-#include "framework.h"
-// SHARED_HANDLERS можно определить в обработчиках фильтров просмотра реализации проекта ATL, эскизов
-// и поиска; позволяет совместно использовать код документа в данным проекте.
-#ifndef SHARED_HANDLERS
+//#include "framework.h"
 #include "AnotherGame.h"
-#endif
-
+//#include <propkey.h>
 #include "AnotherGameDoc.h"
-
-#include <propkey.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
-// CAnotherGameDoc
-
-IMPLEMENT_DYNCREATE(CAnotherGameDoc, CDocument)
-
-BEGIN_MESSAGE_MAP(CAnotherGameDoc, CDocument)
+// CAnotherGameBoard
+IMPLEMENT_DYNCREATE(CAnotherGameBoard, CDocument)
+BEGIN_MESSAGE_MAP(CAnotherGameBoard, CDocument)
 END_MESSAGE_MAP()
 
-
-// Создание или уничтожение CAnotherGameDoc
-
-CAnotherGameDoc::CAnotherGameDoc() noexcept
-{
-	// TODO: добавьте код для одноразового вызова конструктора
-
-}
-
-CAnotherGameDoc::~CAnotherGameDoc()
+// Создание CAnotherGameBoard
+CAnotherGameBoard::CAnotherGameBoard()
 {
 }
 
-BOOL CAnotherGameDoc::OnNewDocument()
+// Уничтожение CAnotherGameBoard
+CAnotherGameBoard::~CAnotherGameBoard()
+{
+}
+
+BOOL CAnotherGameBoard::OnNewDocument()
 {
 	if (!CDocument::OnNewDocument())
 		return FALSE;
 
-	// TODO: добавьте код повторной инициализации
-	// (Документы SDI будут повторно использовать этот документ)
+	// Установка (или сброс) параметров доски
+	m_board.SetupBoard();
 
 	return TRUE;
 }
-
-
-
-
-// Сериализация CAnotherGameDoc
-
-void CAnotherGameDoc::Serialize(CArchive& ar)
-{
-	if (ar.IsStoring())
-	{
-		// TODO: добавьте код сохранения
-	}
-	else
-	{
-		// TODO: добавьте код загрузки
-	}
-}
-
-#ifdef SHARED_HANDLERS
-
-// Поддержка для эскизов
-void CAnotherGameDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
-{
-	// Измените этот код для отображения данных документа
-	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
-
-	CString strText = _T("TODO: implement thumbnail drawing here");
-	LOGFONT lf;
-
-	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
-	pDefaultGUIFont->GetLogFont(&lf);
-	lf.lfHeight = 36;
-
-	CFont fontDraw;
-	fontDraw.CreateFontIndirect(&lf);
-
-	CFont* pOldFont = dc.SelectObject(&fontDraw);
-	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
-	dc.SelectObject(pOldFont);
-}
-
-// Поддержка обработчиков поиска
-void CAnotherGameDoc::InitializeSearchContent()
-{
-	CString strSearchContent;
-	// Задание содержимого поиска из данных документа.
-	// Части содержимого должны разделяться точкой с запятой ";"
-
-	// Например:  strSearchContent = _T("точка;прямоугольник;круг;объект ole;");
-	SetSearchContent(strSearchContent);
-}
-
-void CAnotherGameDoc::SetSearchContent(const CString& value)
-{
-	if (value.IsEmpty())
-	{
-		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
-	}
-	else
-	{
-		CMFCFilterChunkValueImpl *pChunk = nullptr;
-		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
-		if (pChunk != nullptr)
-		{
-			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
-			SetChunkValue(pChunk);
-		}
-	}
-}
-
-#endif // SHARED_HANDLERS
-
-// Диагностика CAnotherGameDoc
-
-#ifdef _DEBUG
-void CAnotherGameDoc::AssertValid() const
-{
-	CDocument::AssertValid();
-}
-
-void CAnotherGameDoc::Dump(CDumpContext& dc) const
-{
-	CDocument::Dump(dc);
-}
-#endif //_DEBUG
-
-
-// Команды CAnotherGameDoc
